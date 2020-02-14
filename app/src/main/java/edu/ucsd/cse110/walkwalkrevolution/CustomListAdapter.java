b1edf79c22d3edbcbb6659ab6755655e1a9e0b2e
unknown <p3liu_ucsd@APPORTO.COM>
2020-02-14 10:30:05 -0800

    updated routelist and route

--- app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/CustomListAdapter.java
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/CustomListAdapter.java
@@ -15,15 +15,15 @@ import androidx.annotation.NonNull;
 import java.util.ArrayList;
 
 class CustomListAdapter extends ArrayAdapter<String> {
-    ArrayList<String[]> routeData;
+    ArrayList<Route> routeData;
     private Activity context;
     private Integer[] favoriteImg;
 
-    public CustomListAdapter(Activity context, ArrayList<String[]> routeData) {
+    public CustomListAdapter(Activity context, ArrayList<Route> routeData) {
         super(context, R.layout.route_list);
         this.routeData = routeData;
-        for(String[] routeValues : routeData) {
-            add(routeValues[0]);
+        for(Route routeValues : routeData) {
+            add(routeValues.getName());
         }
 
         this.context=context;
@@ -43,9 +43,10 @@ class CustomListAdapter extends ArrayAdapter<String> {
         TextView milesText = (TextView) rowView.findViewById(R.id.routeMiles);
         ImageView imageView = (ImageView) rowView.findViewById(R.id.icon);
 
-        nameText.setText(routeData.get(position)[0]);
-        stepsText.setText(routeData.get(position)[2]);
-        milesText.setText(routeData.get(position)[3]);
+        nameText.setText(routeData.get(position).getName());
+        stepsText.setText(routeData.get(position).getSteps());
+        milesText.setText(Float.toString(routeData.get(position).getDistance()));
+
         // Parse Favorite or not
         //imageView.setImageResource(Integer.parseInt(routeData.get(position)[2]));
 
--- app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/RoutesManager.java
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/RoutesManager.java
@@ -12,6 +12,7 @@ import com.google.android.gms.common.util.ArrayUtils;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -39,23 +40,38 @@ public class RoutesManager {
     }
 
     // load saved routes
-    public ArrayList<String[]> loadAll() {
+    public ArrayList<Route> loadAll() {
         Map<String, ?> routeMap = new TreeMap<>(savedRoutesInfo.getAll());
-        ArrayList<String[]> loadList = new ArrayList<String[]>();
+        ArrayList<Route> loadList = new ArrayList<>();
         for (Map.Entry<String, ?> entry : routeMap.entrySet()) {
             loadList.add(loadRoute(entry));
         }
         return loadList;
     }
 
-    public String[] loadRoute(Map.Entry<String, ?> route) {
-        String[] routeValues = {
+    public Route loadRoute(Map.Entry<String, ?> route) {
+        Calendar lastRun = null;
+
+        int year = savedRoutesFeatures.getInt(
+                route.getKey() + R.string.year_tag, -1);
+        int month = savedRoutesFeatures.getInt(
+                route.getKey() + R.string.month_tag, -1);
+        int day = savedRoutesFeatures.getInt(
+                route.getKey() + R.string.day_tag, -1);
+        if (year > 0 && month > 0 && day > 0) {
+            lastRun.set(year, month, day);
+        }
+
+        Route loadedRoute = new Route(
                 route.getKey(),
                 route.getValue().toString(),
-                Integer.toString(savedRoutesFeatures.getInt(
-                        route.getKey() + R.string.steps_tag, 0)),
-                Float.toString(savedRoutesFeatures.getFloat(
-                        route.getKey() + R.string.distance_tag, 0)),
+                savedRoutesFeatures.getInt(
+                        route.getKey() + R.string.steps_tag, 0),
+                savedRoutesFeatures.getFloat(
+                        route.getKey() + R.string.distance_tag, 0),
+                lastRun,
+                savedRoutesFeatures.getString(
+                        route.getKey() + R.string.notes_tag, ""),
                 savedRoutesFeatures.getString(
                         route.getKey() + R.string.path_tag, ""),
                 savedRoutesFeatures.getString(
@@ -66,30 +82,14 @@ public class RoutesManager {
                         route.getKey() + R.string.surface_tag, ""),
                 savedRoutesFeatures.getString(
                         route.getKey() + R.string.difficulty_tag, ""),
-                savedRoutesFeatures.getString(
-                        route.getKey() + R.string.notes_tag, ""),
-                Boolean.toString(savedRoutesFeatures.getBoolean(
-                        route.getKey() + R.string.favorite_tag, false))
-        };
-        return routeValues;
+                savedRoutesFeatures.getBoolean(
+                        route.getKey() + R.string.favorite_tag, false)
+        );
+        return loadedRoute;
     }
 
     // add a new route
     public void addRoute(Route newRoute) {
-        String routeFeatures[] = {
-                newRoute.getName(),
-                newRoute.getStartingPoint(),
-                Integer.toString(newRoute.getSteps()),
-                Float.toString(newRoute.getDistance()),
-                newRoute.getFeatures()[0],
-                newRoute.getFeatures()[1],
-                newRoute.getFeatures()[2],
-                newRoute.getFeatures()[3],
-                newRoute.getFeatures()[4],
-                newRoute.getNotes(),
-                Boolean.toString(newRoute.getFavorite()
-                )};
-
         String startingLocation = newRoute.getStartingPoint();
         infoRouteEditor.putString(newRoute.getName(), startingLocation);
 
@@ -97,6 +97,20 @@ public class RoutesManager {
         featureRouteEditor.putInt(newRoute.getName() + R.string.steps_tag, steps);
         float distance = newRoute.getSteps();
         featureRouteEditor.putFloat(newRoute.getName() + R.string.distance_tag, distance);
+
+        int year = -1;
+        int month = -1;
+        int day = -1;
+
+        if (newRoute.getLastRun() != null) {
+            year = newRoute.getLastRun().get(Calendar.YEAR);
+            month = newRoute.getLastRun().get(Calendar.MONTH);
+            day = newRoute.getLastRun().get(Calendar.DAY_OF_MONTH);
+        }
+
+        featureRouteEditor.putInt(newRoute.getName() + R.string.year_tag, year);
+        featureRouteEditor.putInt(newRoute.getName() + R.string.month_tag, month);
+        featureRouteEditor.putInt(newRoute.getName() + R.string.day_tag, day);
 
         String path = newRoute.getFeatures()[0];
         featureRouteEditor.putString(newRoute.getName() + R.string.path_tag, path);
