c7717c1803494dd6ef6eb5c022fce928b80768a9
Christopher Liu <cmliu@ucsd.edu>
2020-02-14 07:57:09 -0800

    Fix layout and empty bug with height dialog

--- app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/HeightPromptDialog.java
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/HeightPromptDialog.java
@@ -5,8 +5,10 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -18,39 +20,36 @@ public class HeightPromptDialog extends DialogFragment {
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = requireActivity().getLayoutInflater();
-
         SharedPreferences savedHeightPref = getContext().getSharedPreferences("saved_height", getContext().MODE_PRIVATE);
         SharedPreferences.Editor editor = savedHeightPref.edit();
 
         View currentView = inflater.inflate(R.layout.dialog_height_prompt, null);
-        builder.setView(currentView)
-                .setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        saveHeight(currentView, editor);
-                    }
-                })
-                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        cancelHeightInput(editor);
-                    }
-                });
+        builder.setView(currentView);
+        Button bt_confirm = (Button) currentView.findViewById(R.id.bt_confirm);
+        // support mocking
+        bt_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (((EditText) currentView.findViewById(R.id.box_height)).getText().toString().matches("")) {
+                    Toast.makeText(getActivity(), "Please Enter a Valid Height", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    saveHeight(currentView, editor);
+                    dismiss();
+                }
+            }
+        });
         return builder.create();
     }
 
     public void saveHeight(View view, SharedPreferences.Editor editor) {
-        EditText heightInput = (EditText) view.findViewById(R.id.height);
+        EditText heightInput = (EditText) view.findViewById(R.id.box_height);
         float heightInputFloat = Float.valueOf(heightInput.getText().toString());
 
         if (heightInputFloat > 0) {
             editor.putFloat("user_height", heightInputFloat);
             editor.apply();
         }
-    }
-
-    public void cancelHeightInput(SharedPreferences.Editor editor) {
-        editor.putBoolean("use_height", false);
-        editor.apply();
-        HeightPromptDialog.this.getDialog().cancel();
     }
 
     //ResetHeight For Testing Purposes
