da0ad52b89c15d93ce7fae3ac6094b031bcc04ca
Mark Lee <minhoo2000@gmail.com>
2020-02-15 22:34:09 -0800

    Fix unit test for home step count activity

--- app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/HomeActivity.java
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/HomeActivity.java
@@ -1,8 +1,10 @@
 package edu.ucsd.cse110.walkwalkrevolution;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
@@ -20,7 +22,7 @@ import edu.ucsd.cse110.walkwalkrevolution.fitness.GoogleFitAdapter;
 public class HomeActivity extends AppCompatActivity {
 
     public static final String FITNESS_SERVICE_KEY = "FITNESS_SERVICE_KEY";
-    private static final String FITNESS_SERVICE_PROPERTIES = "fitness.properties";
+    public static final String FITNESS_SERVICE_PROPERTIES = "fitness.properties";
 
     private static final String TAG = "HomeActivity";
 
@@ -28,37 +30,23 @@ public class HomeActivity extends AppCompatActivity {
 
     private TextView textSteps;
     private FitnessService fitnessService;
-    private StepCountUpdateAsyncTask task;
+
+    private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            fitnessService.updateStepCount();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         textSteps = findViewById(R.id.textSteps);
 
-        // Read the fitness key from the fitness properties file
-        PropertyReader propertyReader = new PropertyReader(this);
-        Properties properties = propertyReader.getMyProperties(FITNESS_SERVICE_PROPERTIES);
-        String fitnessServiceKey = properties.getProperty(FITNESS_SERVICE_KEY);
-
-        // Create the fitness service using the factory
-        FitnessServiceFactory.put(fitnessServiceKey, new FitnessServiceFactory.BluePrint() {
-            @Override
-            public FitnessService create(HomeActivity HomeActivity) {
-                return new GoogleFitAdapter(HomeActivity);
-            }
-        });
-        fitnessService = FitnessServiceFactory.create(fitnessServiceKey, this);
-        fitnessService.setup();
-
-        if (task == null) {
-            task = new StepCountUpdateAsyncTask();
-            task.execute();
-        }
-
-        Button bt_newRun = (Button) findViewById(R.id.bt_startNewRun);
-
         // Check if user pressed start new run button
+        Button bt_newRun = (Button) findViewById(R.id.bt_startNewRun);
         bt_newRun.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,15 +54,22 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
 
-        Button bt_routes = (Button) findViewById(R.id.bt_gotoRoute2);
-
         // Check if user pressed routes button
+        Button bt_routes = (Button) findViewById(R.id.bt_gotoRoute2);
         bt_routes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 gotoRoutes();
             }
         });
+
+
+        launchFitnessActivity();
+
+        // Register broadcast receiver from step count updater
+        registerReceiver(broadcastReceiver, new IntentFilter(StepCountUpdateService.BROADCAST_ACTION));
+
+        launchUpdateService();
     }
 
     @Override
@@ -89,6 +84,35 @@ public class HomeActivity extends AppCompatActivity {
         } else {
             Log.e(TAG, "ERROR, google fit result code: " + resultCode);
         }
+    }
+
+    public void launchFitnessActivity() {
+        // Read the fitness key from the fitness properties file
+        PropertyReader propertyReader = new PropertyReader(this, FITNESS_SERVICE_PROPERTIES);
+        Properties properties = propertyReader.get();
+        String fitnessServiceKey = properties.getProperty(FITNESS_SERVICE_KEY);
+
+        // Create the fitness service using the factory
+        FitnessServiceFactory.put(fitnessServiceKey, new FitnessServiceFactory.BluePrint() {
+            @Override
+            public FitnessService create(HomeActivity HomeActivity) {
+                return new GoogleFitAdapter(HomeActivity);
+            }
+        });
+
+        // If a fitness service key is passed in, it has higher priority (for testing)
+        Intent intent = getIntent();
+        if (intent.hasExtra(FITNESS_SERVICE_KEY)) {
+            fitnessServiceKey = intent.getStringExtra(FITNESS_SERVICE_KEY);
+        }
+
+        fitnessService = FitnessServiceFactory.create(fitnessServiceKey, this);
+        fitnessService.setup();
+    }
+
+    public void launchUpdateService() {
+        Intent intent = new Intent(this, StepCountUpdateService.class);
+        startService(intent);
     }
 
     public void gotoNewRun() {
@@ -109,22 +133,6 @@ public class HomeActivity extends AppCompatActivity {
     public long getStepCount() {
         fitnessService.updateStepCount();
         return lastStepCount;
-    }
-
-    private class StepCountUpdateAsyncTask extends AsyncTask<String, String, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            while (!isCancelled()) {
-                fitnessService.updateStepCount();
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-            return null;
-        }
     }
 }
 
--- app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/PropertyReader.java
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/PropertyReader.java
@@ -3,27 +3,31 @@ package edu.ucsd.cse110.walkwalkrevolution;
 import android.content.Context;
 import android.content.res.AssetManager;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertyReader {
     private Context context;
     private Properties properties;
+    private String file;
 
-    public PropertyReader(Context context) {
+    public PropertyReader(Context context, String file) {
         this.context = context;
-        properties = new Properties();
+        this.file = file;
     }
 
-    public Properties getMyProperties(String file) {
+    public Properties get() {
         try {
             AssetManager assetManager = context.getAssets();
             InputStream inputStream = assetManager.open(file);
+            properties = new Properties();
             properties.load(inputStream);
         } catch (Exception e){
             System.out.print(e.getMessage());
+            properties = null;
         }
-
         return properties;
     }
 }
