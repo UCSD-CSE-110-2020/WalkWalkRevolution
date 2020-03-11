9f0cca7e39efe146a9934d9e5eb5e0b2f033c0f8
Mark Lee <minhoo2000@gmail.com>
2020-03-10 17:14:14 -0700

    Add user login information to Firebase

--- app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/WalkWalkRevolutionApplication.java
+++ app/src/main/java/edu/ucsd/cse110/walkwalkrevolution/WalkWalkRevolutionApplication.java
@@ -2,6 +2,20 @@ package edu.ucsd.cse110.walkwalkrevolution;
 
 import android.app.Application;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import edu.ucsd.cse110.walkwalkrevolution.firebase.FirebaseFirestoreAdapter;
+
 public class WalkWalkRevolutionApplication extends Application {
     public static StepCountData stepCount = new StepCountData();
+    public static FirebaseFirestoreAdapter adapter;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(getApplicationContext());
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        adapter = new FirebaseFirestoreAdapter(db);
+    }
 }
